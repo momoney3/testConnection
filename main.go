996324ee7141a2dce717b9b9540d57b2411b35d3
wil <119863549+momoney3@users.noreply.github.com>
2024-06-28 00:04:39 +0100

    this is the new one

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"syscall"
 )
 
-type Error struct {
-	Code         ErrNo         /* The error code returned by SQLite */
-	ExtendedCode ErrNoExtended /* The extended error code returned by SQLite */
-	SystemErrno  syscall.Errno /* The system errno returned by the OS through SQLite, if applicable */
-	// contains filtered or unexported fields
-}
+//type Error struct {
+//	Code         ErrNo         /* The error code returned by SQLite */
+//	ExtendedCode ErrNoExtended /* The extended error code returned by SQLite */
+//	SystemErrno  syscall.Errno /* The system errno returned by the OS through SQLite, if applicable */
+//	// contains filtered or unexported fields
+//}
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println("this is the new one")
 }
