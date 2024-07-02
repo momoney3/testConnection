12ea72ef9f712c28466f5defc00528053ee83746
wil <119863549+momoney3@users.noreply.github.com>
2024-07-03 00:10:29 +0100

    update the main go file

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
 )
 
 func main() {
-	fmt.Println("hello world")
+	dataPath := "./OMCDateBase.db"
+	db, err := sql.Open("sqlite3", dataPath)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Update tables
+	sqlInsert := `
+  INSERT INTO DataSources (Name)
+  VALUES (?)`
+	_, err = db.Exec(sqlInsert, "DS-Test")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Insertion successful")
 }
