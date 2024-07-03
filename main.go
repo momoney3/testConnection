aa719a0a6e8fea48c752254d124d280ce34640b5
wil <119863549+momoney3@users.noreply.github.com>
2024-07-04 00:25:23 +0100

    added more sql to main

--- main.go
+++ main.go
@@ -8,20 +8,44 @@ import (
 )
 
 func main() {
-	dataPath := "./OMCDateBase.db"
+	dataPath := "./OMCDataBase.db"
 	db, err := sql.Open("sqlite3", dataPath)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Create a new tabel
-	t
-	// Update tables
+	// Alter table
+	sqlAlter := `
+  ALTER TABLE tt
+  ADD COLUMN`
+	_, err = db.Exec(sqlInsert)
+	if err != nil {
+		panic(err)
+	}
+
+	// Retrieve data
+	sqlAlter := `
+  SELECT * FROM DataSources 
+  ADD COLUMN;`
+	_, err = db.Exec(sqlInsert)
+	if err != nil {
+		panic(err)
+	}
+
+	// Insert data in tables
 	sqlInsert := `
-  INSERT INTO DataSources (Name)
-  VALUES (?);`
-	_, err = db.Exec(sqlInsert, "DSeTest")
+  INSERT INTO DataSources (ID, Name, Agency, DataType)
+  VALUES ($1, $2, $3, $4);`
+	_, err = db.Exec(sqlInsert, 4244, "DS-test-go", "goTest", "smb")
+	if err != nil {
+		panic(err)
+	}
+
+	// Select
+	sqlSelect := `
+  SELECT * FROM ConnetionsAndScan WHERE LastTest > 1;`
+	_, err = db.Exec(sqlSelect)
 	if err != nil {
 		panic(err)
 	}
