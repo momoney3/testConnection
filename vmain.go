b34ec5576b16b6a8d70c33a24a46477dadc1d1ca
wil <119863549+momoney3@users.noreply.github.com>
2024-07-11 23:16:13 +0100

    added new linter

--- vmain.go
+++ vmain.go
@@ -5,7 +5,31 @@ import (
 )
 
 // NOTE: Creat a function to dispaly the data.
-// func displayData(db)
+func displayData(db *sql.DB) ([]Album, error) {
+	rows, err := db.Query("SELECT * FROM album", artist)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Slice to hold data from returned rows.
+	var albums []Album
+
+	// Loop through rows, using scan s to assign column data to struct fiels.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist,
+			&alb.Price); err != nil {
+			return albums, err
+		}
+		albums = append(albums, alb)
+	}
+	if err = rows.Err(); err != nil {
+		return albums, err
+	}
+	return albums, nil
+}
+
 func main() {
 	dataPath := "./OMCDataBase.db"
 	db, err := sql.Open("sqlite3", dataPath)
@@ -13,12 +37,4 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-
-
-
-  rows, err 
-
-
-
-  
 }
